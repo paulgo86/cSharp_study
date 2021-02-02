bc2198742986dff2649e92645993129d34af9796
paulgo86 <ghseok86@gmail.com>
2021-02-02 23:46:40 +0900

    ch06 ongoing

--- ch06_method/Program.cs
+++ ch06_method/Program.cs
@@ -8,8 +8,24 @@ namespace ch06_method
 {
     class Program
     {
+        class Hoi
+        {
+            string name;
+            public Hoi(string name)
+            {
+                this.name = name;
+                Console.WriteLine(this.name + " 호이 생성~");
+            }
+            ~Hoi() {
+                Console.WriteLine(this.name+" 호이 소멸~");
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Hoi h = new Hoi("호호호");
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
         }
     }
 }
